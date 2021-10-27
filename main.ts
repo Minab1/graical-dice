a0163d8e734b63c6e800c38ae4eed31a281fa6ae
Minab1 <92942758+Minab1@users.noreply.github.com>
2021-10-27 12:55:27 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,54 @@
+let nuber = 0
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    nuber = randint(0, 6)
+    if (nuber == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (nuber == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . #
+            . . . . .
+            . . . . .
+            `)
+    } else if (nuber == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (nuber == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (nuber == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+})
